62ecbdfce90398a4e2b9fd549467c7b32cccd04c
ezedan001 <ezed923@gmail.com>
2025-09-27 16:38:11 +0100

    Add files via upload
    Signed-off-by: ezedan001 <ezed923@gmail.com>

--- javascript.js
+++ javascript.js
@@ -1,6 +1,50 @@
-const toggle = document.getElementById("menu-toggle");
-const navMenu = document.querySelector(".nav-menu");
-
-toggle.addEventListener("click", () => {
-  navMenu.classList.toggle("active");
+// Sticky scroll effect
+window.addEventListener("scroll", function () {
+    const navbar = document.querySelector(".navbar");
+    navbar.classList.toggle("scrolled", window.scrollY > 50);
 });
+
+// Mobile menu toggle
+const menuToggle = document.getElementById("menu-toggle");
+const navLinks = document.getElementById("nav-links");
+
+menuToggle.addEventListener("click", () => {
+    navLinks.classList.toggle("open");
+    menuToggle.classList.toggle("active");
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
